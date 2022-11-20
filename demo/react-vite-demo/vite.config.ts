da9edcafda97ec33d4b689931cfbc2dc9898411f
Davide Ghilardi <ghilardi.davide@gmail.com>
2022-11-20 19:01:51 +0100

    add proxies in vite

--- demo/react-vite-demo/vite.config.ts
+++ demo/react-vite-demo/vite.config.ts
@@ -3,5 +3,24 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/room': {
+        target: 'http://localhost:3040',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/write': {
+        target: 'http://localhost:3040',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/read': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
