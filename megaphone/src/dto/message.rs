94213c5177d80a0475304001dfaecf4c68b83d67
Davide Ghilardi <ghilardi.davide@gmail.com>
2023-10-29 08:32:31 +0100

    add randomic event-id for each received event

--- megaphone/src/dto/message.rs
+++ megaphone/src/dto/message.rs
@@ -1,9 +1,13 @@
+use rand::{random, Rng};
+use rand::distributions::Alphanumeric;
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct EventDto {
     #[serde(rename = "sid")]
     pub stream_id: String,
+    #[serde(rename = "eid")]
+    pub event_id: String,
     pub body: serde_json::Value,
 }
 
@@ -14,6 +18,11 @@ impl EventDto {
     ) -> Self {
         Self {
             stream_id,
+            event_id: rand::thread_rng()
+                .sample_iter(&Alphanumeric)
+                .take(23)
+                .map(char::from)
+                .collect(),
             body,
         }
     }
