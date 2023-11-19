1b37669fc6225410a4d578c6e7276d82ee28ff49
Davide Ghilardi <ghilardi.davide@gmail.com>
2023-11-19 04:59:37 +0100

    update chat demo to support batch requests

--- demo/deno/chat-server.ts
+++ demo/deno/chat-server.ts
@@ -1,10 +1,20 @@
 #! /usr/bin/env -S deno run --allow-net --allow-env
 
-import { Application, Router } from 'https://deno.land/x/oak@v11.1.0/mod.ts';
+import { Application, Router } from 'https://deno.land/x/oak@v12.6.1/mod.ts';
 
 interface ChannelParams {
   channelId: string,
   agentName: string,
+}
+
+interface WriteBatchResDto {
+  failures: MessageDeliveryFailure[],
+}
+
+interface MessageDeliveryFailure {
+  channel: string,
+  index: number,
+  reason: string,
 }
 
 const megaphoneUrl = Deno.env.get("MEGAPHONE_URL") || 'http://localhost:3000';
@@ -16,14 +26,21 @@ const rooms: Record<string, ChannelParams[]> = {};
 
 const router = new Router();
 
-router.post('/room/:room', async (ctx) => {
-  const { channelId, agentName }: ChannelParams = await fetch(`${megaphoneUrl}/create`, { method: 'POST' })
+async function createChannel(): Promise<string> {
+  const { channelId }: ChannelParams = await fetch(`${megaphoneUrl}/create`, { method: 'POST' })
     .then((resp) => {
       if (!resp.ok) {
         throw new Error("HTTP status code: " + resp.status);
       }
       return resp.json();
     });
+
+  return channelId;
+}
+
+router.post('/room/:room', async (ctx) => {
+  const channelId = ctx.request.headers.get('use-channel') || await createChannel();
+  const agentName = channelId.split('.')[0];
 
   const subscriptions = rooms[ctx.params.room];
   if (subscriptions) {
@@ -47,28 +64,48 @@ router.post('/send/:room', async (ctx) => {
   const req = await ctx.request.body({ type: 'json' }).value;
   const timestamp = new Date().toISOString();
 
-  const unavailableChannels: number[] = [];
-  const promises = subscriptions
-    .map(({ channelId, agentName }, idx) => fetch(`${megaphoneAgentUrlTemplate.replace('%agentName%', agentName)}/write/${channelId}/new-message`, {
+  const unavailableChannels: string[] = [];
+  const groupedSubs = subscriptions
+    .reduce((acc, current) => {
+      if (acc[current.agentName]) {
+        acc[current.agentName] = [...acc[current.agentName], current];
+      } else {
+        acc[current.agentName] = [current];
+      }
+      return acc;
+    }, {} as Record<string, ChannelParams[]>)
+  const promises = Object.entries(groupedSubs)
+    .map(([agentName, channels]) => fetch(`${megaphoneAgentUrlTemplate.replace('%agentName%', agentName)}/write-batch`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        sender: req.sender,
-        timestamp,
-        message: req.message,
+        channelIds: channels.map(channelParams => channelParams.channelId),
+        messages: [{
+          streamId: 'new-message',
+          body: {
+            room: ctx.params.room,
+            sender: req.sender,
+            timestamp,
+            message: req.message,
+          }
+        }]
       }),
     }).then(res => {
       if (res.status == 404) {
-        console.warn(`subscription ${channelUuid} not found`);
-        unavailableChannels.push(idx);
+        console.warn(`subscription ${agentName} not found`);
+        unavailableChannels.push(...channels.map(par => par.channelId))
       }
+      return res.json()
+    }).then((res: WriteBatchResDto) => {
+      unavailableChannels.push(...res.failures.map(f => f.channel));
     }))
     ;
 
   await Promise.all(promises);
-  unavailableChannels
-    .sort((a,b) => b - a)
-    .forEach(idx => subscriptions.splice(idx, 1));
+  for (const room of Object.keys(rooms)) {
+    rooms[room] = rooms[room]
+      .filter(c => !unavailableChannels.includes(c.channelId));
+  }
 
   ctx.response.body = JSON.stringify({ status: 'ok' });
 });
