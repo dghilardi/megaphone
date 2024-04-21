03f5bfc47a9780fa5c96d8f90c450471a86d241d
Davide Ghilardi <ghilardi.davide@gmail.com>
2024-04-21 16:09:36 +0200

    handle http responses

--- tests/client.rs
+++ tests/client.rs
@@ -18,25 +18,37 @@ impl MegaphoneRestClient {
 
     pub async fn create(&self, req: &ChannelCreateReqDto) -> anyhow::Result<ChannelCreateResDto> {
         let client = reqwest::Client::new();
-        client.post(format!("http://{}:{}/create", self.host, self.port))
+        let resp = client.post(format!("http://{}:{}/create", self.host, self.port))
             .json(req)
             .send()
             .await
-            .context("Error during channel create")?
-            .json::<ChannelCreateResDto>()
-            .await
-            .context("Error parsing response")
+            .context("Error during channel create")?;
+
+        if resp.status().is_success() {
+            resp
+                .json::<ChannelCreateResDto>()
+                .await
+                .context("Error parsing response")
+        } else {
+            anyhow::bail!("Http failed with {}", resp.status())
+        }
     }
 
     pub async fn write(&self, producer_address: &str, stream_id: &str, payload: impl Serialize) -> anyhow::Result<BasicOutcomeDto> {
         let client = reqwest::Client::new();
-        client.post(format!("http://{}:{}/write/{producer_address}/{stream_id}", self.host, self.port))
+        let resp = client.post(format!("http://{}:{}/write/{producer_address}/{stream_id}", self.host, self.port))
             .json(&payload)
             .send()
             .await
-            .context("Error during channel write")?
-            .json()
-            .await
-            .context("Error parsing response")
+            .context("Error during channel write")?;
+        
+        if resp.status().is_success() {
+            resp
+                .json()
+                .await
+                .context("Error parsing response")
+        } else {
+            anyhow::bail!("Http failed with {}", resp.status())
+        }
     }
 }
