ded4114d9cf2c9247e6901ecea9198fa8d16a594
Davide Ghilardi <ghilardi.davide@gmail.com>
2024-04-21 20:44:09 +0200

    single cluster for multiple tests

--- tests/single_node_integration_test.rs
+++ tests/single_node_integration_test.rs
@@ -1,13 +1,16 @@
+use std::sync::OnceLock;
 use lazy_static::lazy_static;
 use megaphone::dto::agent::OutcomeStatus;
 use megaphone::dto::channel::ChannelCreateReqDto;
 use megaphone::model::constants::protocols::HTTP_STREAM_NDJSON_V1;
 use serde_json::json;
 use testcontainers::clients::Cli;
+use testcontainers::Container;
 
 use crate::client::MegaphoneRestClient;
 use crate::kubernetes::cluster::prepare_cluster;
 use crate::testcontainers_ext::k3s;
+use crate::testcontainers_ext::k3s::K3s;
 
 mod testcontainers_ext;
 mod kubernetes;
@@ -17,13 +20,29 @@ mod client;
 lazy_static! {
     static ref AIRGAP_DIR: tempfile::TempDir = tempfile::tempdir().expect("Error creating airgap temp dir");
     static ref K3S_CONF_DIR: tempfile::TempDir = tempfile::tempdir().expect("Error creating conf temp dir");
+
+    static ref DOCKER: Cli = Cli::default();
+}
+static CONTAINER: OnceLock<Container<'static, K3s>> = OnceLock::new();
+
+async fn get_container() -> anyhow::Result<&'static Container<'static, K3s>> {
+    let result = if let Some(container) = CONTAINER.get() {
+        container
+    } else {
+        let container = prepare_cluster(&DOCKER, AIRGAP_DIR.path()).await?;
+        CONTAINER.set(container)
+            .map_err(|_| anyhow::anyhow!("Cannot set oncelock"))?;
+        let container = CONTAINER.get()
+            .ok_or_else(|| anyhow::anyhow!("Oncelock not valorized"))?;
+        container
+    };
+    Ok(result)
 }
 
 #[tokio::test]
 #[serial_test::serial]
 async fn channel_create() {
-    let docker = Cli::default();
-    let container = prepare_cluster(&docker, AIRGAP_DIR.path())
+    let container = get_container()
         .await
         .expect("Error creating megaphone cluster");
 
@@ -40,8 +59,7 @@ async fn channel_create() {
 #[tokio::test]
 #[serial_test::serial]
 async fn channel_write() {
-    let docker = Cli::default();
-    let container = prepare_cluster(&docker, AIRGAP_DIR.path())
+    let container = get_container()
         .await
         .expect("Error creating megaphone cluster");
 
